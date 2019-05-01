8d724c496cb86a1856e99a1b6ae57d8ea86333b9
henry <henryharfouche@gmail.com>
2019-05-01 13:30:19 -0400

    Updated functions for efficiency purposes. Code runs 20X faster now

--- streak_counter.py
+++ streak_counter.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 
-def hit_checker(df,year,month,day, players, game_numb = [[0,1],[0,1]]):
+def hit_checker(df,players, game_numb = [[0,1],[0,1]]):
+#May 1st, 2019. Parameters changed: need to pass a dataframe already organized by year month day. 20x faster to run.
+#Organizing this dataframe can be accomplished outside hit_checker by using pandas .groupby and .get_groups(dates) functions.
+
     results =[]
     hit_flags = [20, 21, 22, 23]   #20 = single, 21 = double, 22 = triple, 23 = homerun
-    df = df[df['year']==year]
-    df = df[df['month'] == month]
-    df = df[df['day_'] == day]
     i = 0
     for player in players:
         new_df = df[df.double_header_flag.isin(game_numb[i])]
@@ -15,14 +15,13 @@ def hit_checker(df,year,month,day, players, game_numb = [[0,1],[0,1]]):
             results.append(int(sum(new_df.event_type.isin(hit_flags))>=1))
         i = i + 1
 
-
     if len(results) ==0:     #all the players either did not play or got charged 0 at bats so the streak should continue
         return 'continue_streak'
 
-    if sum(results) < len(results):
+    elif sum(results) < len(results):
         return 'lose_streak'
 
-    if sum(results) == len(results):
+    elif sum(results) == len(results):
         return sum(results)
 
     # Same function as below except returns 'continue_streak', 'lose_streak' or the number your stream should extend by
@@ -31,13 +30,10 @@ def hit_checker(df,year,month,day, players, game_numb = [[0,1],[0,1]]):
 #the game number if its a double header. BTS rules specify that it will assume its game #1 if the user doesn't specify which game number
 #game number will have to be specified as lists in lists.
 
-def streak_counter(df,year,month,day, players, streak, game_numb = [[0,1],[0,1]]):
+def streak_counter(df,players, streak, game_numb = [[0,1],[0,1]]):
 
     results =[]
     hit_flags = [20, 21, 22, 23]   #20 = single, 21 = double, 22 = triple, 23 = homerun
-    df = df[df['year']==year]
-    df = df[df['month'] == month]
-    df = df[df['day_'] == day]
     i = 0
     for player in players:
         new_df = df[df.double_header_flag.isin(game_numb[i])]
@@ -50,12 +46,12 @@ def streak_counter(df,year,month,day, players, streak, game_numb = [[0,1],[0,1]]
         #return 'continue_streak'
         return streak
 
-    if sum(results) < len(results):
+    elif sum(results) < len(results):
         #return 'lose_streak'
         streak = 0
         return streak
 
-    if sum(results) == len(results):
+    elif sum(results) == len(results):
         streak = streak + sum(results)
         return streak
 
@@ -67,7 +63,6 @@ def streak_counter(df,year,month,day, players, streak, game_numb = [[0,1],[0,1]]
 #(remember results only expands if the player got an AB)
 
 #since results is 1 or 0 in the last case the sum equals the lenght so it should return the new streak counter
-
 
 
 # -*- coding: utf-8 -*-
